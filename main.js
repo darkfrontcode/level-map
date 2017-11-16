513ab6229511b9447f067427c48e42f91902bf50
Ueslei de Souza <ueslei.souza@cwi.com.br>
2017-11-16 17:39:08 -0200

    two paths

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ console.clear()
 
 const button = document.getElementById("button-action") 
 const line = document.getElementById("line")
+const lineTwo = document.getElementById("line-two")
 const balloon = document.getElementById("balloon")
 const path = MorphSVGPlugin.pathDataToBezier(line, { align: balloon })
+const pathTwo = MorphSVGPlugin.pathDataToBezier(lineTwo, { align: balloon })
 const update = () => {
 	// console.log(this.progress())
 }
@@ -15,6 +17,7 @@ button.onclick = () => {
 	.set(balloon, { xPercent:-50, yPercent:-50, transformOrigin:"50% 50%" })
 	.add[
 		TweenLite.to(balloon, 2, { bezier: { values:path, type:"cubic", autoRotate: true }}),
+		TweenLite.to(balloon, 2, { bezier: { values:pathTwo, type:"cubic", autoRotate: true }, delay: 2}),
 		TweenMax.fromTo(line, 2, { drawSVG: '0% 0%' }, { drawSVG: '0% 100%' })
 	]
 }
