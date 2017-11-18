2321213a10fc52e860d35731f5095c0325c095dd
Ueslei de Souza <ueslei.souza@cwi.com.br>
2017-11-18 13:48:10 -0200

    updates

--- index.ts
+++ index.ts
@@ -8,9 +8,7 @@ const port = process.env.PORT || 3000
 const environment = process.env.NODE_ENV || 'development'
 
 app.use(express.static(path.join(__dirname, './public')))
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/map.html')))
-app.get('main.js', (req, res) => res.sendFile(path.join(__dirname, './public/main.js')))
 
 app.listen(port, err => {
 	if(err) return console.log(err)
