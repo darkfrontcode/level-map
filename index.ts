2f86ab59f2a1f532c6e99eb816d2d784f1839540
Ueslei de Souza <ueslei.souza@cwi.com.br>
2017-11-18 13:45:16 -0200

    ts-node running

--- index.ts
+++ index.ts
@@ -4,12 +4,13 @@ const app = express()
 const chalk = require('chalk')
 const log = console.log
 
-const port = process.env.PORT || 1001
+const port = process.env.PORT || 3000
 const environment = process.env.NODE_ENV || 'development'
 
 app.use(express.static(path.join(__dirname, './public')))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../views/map.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/map.html')))
+app.get('main.js', (req, res) => res.sendFile(path.join(__dirname, './public/main.js')))
 
 app.listen(port, err => {
 	if(err) return console.log(err)
