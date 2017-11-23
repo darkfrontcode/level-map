6af2d8f96f0e534bbaf06428b7180e7ad9db69c3
Ueslei de Souza <ueslei.souza@cwi.com.br>
2017-11-23 14:32:36 -0200

    thoughts

--- source/breadth-first-search.ts
+++ source/breadth-first-search.ts
@@ -4,7 +4,10 @@ class Level
 {
 	public value:number
 	public parent:number
-	public children:Array<number>
+	private _children:Array<number>
+	public singleChild:boolean
+	public hasChildren:boolean
+	public visited:boolean
 	
 	constructor(
 		value:number, 
@@ -16,82 +19,125 @@ class Level
 		this.parent = parent
 		this.children = children
 	}
-}
 
-const list = new Array<Level>(
-	new Level(0, null, [1]),
-	new Level(1, 0, [2]),
-	new Level(2, 1, [3, 4]),
-	new Level(3, 2, null),
-	new Level(4, 2, [5]),
-	new Level(5, 4, null),
-)
-
-const search = (target:number) => {
-
-	let current = list[0]
-	let visited = new Array<number>()
-	let loop = 0
-
-	const visit = (level:Level) => {
-		if(!visited.find(v => v == level.value)) visited.push(level.value)
+	get children()
+	{
+		return this._children
 	}
 
-	while(true)
+	set children(children:Array<number>)
 	{
-		if(current.value == target)
+		if(children.length > 0)
 		{
-			visit(current)
-			break
+			this.hasChildren = true
+			this.singleChild = children.length == 1
 		}
 		else
 		{
-			if(current.children)
+			this.hasChildren = false
+		}
+
+		this._children = children
+	}
+}
+
+class Scenario
+{
+	public static levels = new Array<Level>(
+		new Level(0, null, [1]),
+			new Level(1, 0, [2]),
+				new Level(2, 1, [3, 5, 6]),
+					new Level(3, 2, [4]),
+						new Level(4, 3),
+					new Level(5, 2),
+					new Level(6, 2, [7, 8]),
+						new Level(7, 6),
+						new Level(8, 6),
+	)
+
+	public static clone(levels:Array<Level>) : Array<Level>
+	{
+		return { ...levels }
+	}
+
+	public static search(target:number)
+	{
+		const levels = this.clone(this.levels)
+		const visited = new Array<number>()
+		let current = levels[0]
+		let loop = 0
+	
+		const visit = (level:Level) => {
+			if(!level.visited)
 			{
-				if(current.children.length == 1)
+				visited.push(level.value)
+				level.visited = true
+			}
+		}
+	
+		while(true)
+		{
+			if(current.value == target)
+			{
+				visit(current)
+				break
+			}
+			else
+			{
+				if(current.hasChildren)
 				{
-					visit(current)
-					current = list[current.children[0]]
+					if(current.singleChild)
+					{
+						visit(current)
+						current = levels[current.children[0]]
+					}
+					else
+					{
+						visit(current)
+						
+						for(let child of current.children)
+						{	
+							if(!visited.find(v => v == child))
+							{
+								visited.push(child)
+								current = levels[child]
+								break
+							}
+						}
+					}
 				}
 				else
 				{
 					visit(current)
-					for(let child of current.children)
-					{	
-						if(!visited.find(v => v == child))
-						{
-							visited.push(child)
-							current = list[child]
-							break
-						}
-					}
+					current = levels[current.parent]
 				}
 			}
-			else
-			{
-				visit(current)
-				current = list[current.parent]
-			}
+
+			loop++
+			if(loop == 15) break
 		}
+	
+		// const path = new Array<number>()
+		// let p = visited[visited.length - 1]
+	
+		// while(true)
+		// {
+		// 	if(p == 0)
+		// 	{
+		// 		// TODO:
+		// 		break
+		// 	}
+		// 	else
+		// 	{
+		// 		const parent = this.levels[p].parent
+		// 		path.unshift(parent)
+		// 		p = parent
+		// 	}
+		// }
 
-		loop++
-		if(loop == list.length) break
+		return visited
 	}
-
-	const path = new Array<number>()
-	let p = visited[visited.length - 1]
-
-	while(p != 0)
-	{
-		const parent = list[p].parent
-		path.unshift(parent)
-		p = parent
-	}
-
-	console.log(path)
-
-	return visited
 
 }
 
-console.log(search(5))
+console.log(Scenario.search(5))
