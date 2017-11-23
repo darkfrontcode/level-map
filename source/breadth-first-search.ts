b6a3fa85e0d0ede4b001777e414bead17a894687
Ueslei de Souza <ueslei.souza@cwi.com.br>
2017-11-23 21:12:04 -0200

    pre-levels ideia

--- source/breadth-first-search.ts
+++ source/breadth-first-search.ts
@@ -100,18 +100,83 @@ class Scenario
 
 }
 
-console.log(Scenario.findPath(8))
+// console.log(Scenario.findPath(8))
+
+
+class PreLevel
+{
+	public value:number
+	public parent:number
+	public children: Array<number>
+
+	constructor(
+		value:number, 
+		parent:number = null, 
+		children:Array<number> = new Array<number>()
+	) 
+	{
+		this.value = value
+		this.parent = parent
+		this.children = children
+	}
+}
+
+class PreLevels
+{
+	public list:Array<Level>
+
+	constructor(preLevels:Array<PreLevel>)
+	{
+		this.list = new Array<Level>()
+		this.buildList(preLevels)
+		this.buildValues(preLevels)
+	}
+
+	private buildList(preLevels:Array<PreLevel>) : void
+	{
+		for(let preLevel of preLevels)
+		{
+			this.list.push(new Level(preLevel.value))
+		}
+	}
+
+	private buildValues(preLevels:Array<PreLevel>) : void
+	{
+		for(let preLevel of preLevels)
+		{
+			const level = this.list[preLevel.value]
+			const parent = this.list[preLevel.parent]
+
+			level.addParent(parent)
+			preLevel.children.map(c => level.addChild(this.list[c]))
+		}
+	}
+}
+
+const preLevels = new PreLevels(new Array<PreLevel>(
+	new PreLevel(0, null, [1]),
+	new PreLevel(1, 0, [2]),
+	new PreLevel(2, 1, [3,5,6]),
+	new PreLevel(3, 2, [4]),
+	new PreLevel(4, 3),
+	new PreLevel(5, 2),
+	new PreLevel(6, 2, [7, 8]),
+	new PreLevel(7, 6),
+	new PreLevel(8, 6),
+))
+
+console.log(preLevels.list)
 
 /*
 
-	0
-	----1
-	--------2
-	------------3
-	----------------4
-	------------5
-	------------6
-	----------------7
-	----------------8
+	zero
+		one
+			two
+				tree
+					four
+				five
+				six
+					seven
+					eighth
 
 */
